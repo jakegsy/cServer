d6fa9d900c1d253308ebfdf5c3ab2a32c63afe59
jakegsy <siyuangoh@gmail.com>
2017-04-25 12:16:26 +0800

    Initial Commit

---
+++ cServer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cServer
+//
+//  Created by Jake Goh on 25/4/17.
+//  Copyright © 2017 Jake Goh Si Yuan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
